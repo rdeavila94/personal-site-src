598f13a0d9b7b9b321b7f3228a7a83011303fbd4
rdeavila94 <crayonsrcool@hotmail.com>
2017-09-12 16:56:08 -0700

    Changed animation speed. Will begin working on header redesign.

--- app/directives/delayed-exit.directive.ts
+++ app/directives/delayed-exit.directive.ts
@@ -9,11 +9,9 @@ export class DelayedExitDirective {
     /*if condition is true (course has been selected) wait before loading it*/
     if (condition) {
       setTimeout(() => {
-        console.log('inside create');
         this.vcRef.createEmbeddedView(this.templateRef);
       }, 500);
     } else {
-        console.log('inside clearview');
         setTimeout(() => {
           this.vcRef.clear();
         }, 500);
