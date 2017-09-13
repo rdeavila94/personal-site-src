6a556f9012138c4c96d91446c9b9e257d95b34ed
rdeavila94 <crayonsrcool@hotmail.com>
2017-09-13 00:10:12 -0700

    Skeleton largely complete. Just inputting data.
    Fill in with the content.

--- app/projects/projects.component.ts
+++ app/projects/projects.component.ts
@@ -8,7 +8,7 @@ import {Project} from '../models/project.model';
 })
 export class ProjectsComponent implements OnInit {
   @Input('change') change: boolean;
-  projectsList = [new Project('Personal site', 'I built this website using Angular4 and Bootstrap. I was in need of a personal website to serve as my portal of myself to the world. I learned Angular for this project and it has become my front end framework of choice.  Will attempt to include server side code to make a functional contact form here, bypassing the need for emails.', 'assets/Capture.JPG', 'assets/Capture.JPG'), new Project('CRM', 'cscsc', 'assets/crm.jpg', 'assets/crm.gif')];
+  projectsList = [new Project('Personal site', 'I built this website using Angular4 and Bootstrap. I was in need of a personal website to serve as my portal of myself to the world. I learned Angular for this project and it has become my front end framework of choice.  Will attempt to include server side code to make a functional contact form here, bypassing the need for emails.', 'assets/Capture.JPG', 'assets/Capture.JPG'), new Project('CRM', 'Aspects for logging data. Includes validation of input. Performs CRUD operations.', 'assets/crm.jpg', 'assets/crm.gif')];
 
   constructor() {}
 
