0f0e22b5b764a2bb600211532658d852776ebe71
rdeavila94 <crayonsrcool@hotmail.com>
2017-09-16 12:07:09 -0700

    Going to try to make Projects columns of equal height.

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,8 +6,7 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  title = 'app';
-  colorscale = {color1: '#000', color2: '#bfbfbf', color3: '#808080', color4: '#3f3f3f', color5: '#fff'};
+  title = 'personalsite';
   page: number;
 
   ngOnInit() {
